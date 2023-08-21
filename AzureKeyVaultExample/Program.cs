d36e1f17959714da165b4435a7342ba2065d9776
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-08-21 19:48:11 +0100

    CHANGE: Program.cs updated for key vault config

--- AzureKeyVaultExample/Program.cs
+++ AzureKeyVaultExample/Program.cs
@@ -1,4 +1,19 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string keyVaultUrl = builder.Configuration["AzurekeyVaultConfig:KVUrl"];
+string tenantId = builder.Configuration["AzurekeyVaultConfig:KVTenantId"];
+string clientId = builder.Configuration["AzurekeyVaultConfig:KVClientId"];
+string clientSecretId = builder.Configuration["AzurekeyVaultConfig:KVClientSecretId"];
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddEnvironmentVariables();
+
+var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecretId);
+var secretClient = new SecretClient(new Uri(keyVaultUrl), clientSecretCredential);
 
 // Add services to the container.
 
